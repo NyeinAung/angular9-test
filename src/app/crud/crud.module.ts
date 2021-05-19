bb95498413846f348b78d66f26d03f7b4978eec6
Nyein Aung <nyeinaung1995@gmail.com>
2021-05-20 00:03:20 +0630

    init project

---
+++ src/app/crud/crud.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { CrudRoutingModule } from './crud-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CrudRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ]
+})
+export class CrudModule { }
